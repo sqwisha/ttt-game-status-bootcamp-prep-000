2c25fad2bbe1236928697089f37edceeb4e6925a
S H <sashanicolehart@gmail.com>
2018-09-12 23:42:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 
   
 def won?(board)
-  WIN_COMBINATIONS.select do |win_combination|
+  WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2] 
@@ -29,7 +29,7 @@ def won?(board)
         if position_1 == "X" || position_1 == "O"
           return win_combination
         else
-          false
+          return false
         end
     end
   end
